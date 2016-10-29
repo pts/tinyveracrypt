78759be8d38273e0b619fa2013d1731a788da409
Peter Szabo <pts@fazekas.hu>
2016-10-29 18:39:41 +0100

    updated tinyveracrypt.py

--- tinyveracrypt.py
+++ tinyveracrypt.py
@@ -2,7 +2,101 @@
 # by pts@fazekas.hu at Fri Oct 28 16:32:11 CEST 2016
 
 import binascii
+import itertools
 import struct
+import sys
+
+import CryptoPlus.Cipher.python_AES  # !! Embed.
+
+# Helpers for do_hmac.
+hmac_trans_5C = ''.join(chr(x ^ 0x5C) for x in xrange(256))
+hmac_trans_36 = ''.join(chr(x ^ 0x36) for x in xrange(256))
+
+
+# Faster than `import hmac' because of less indirection.
+def do_hmac(key, msg, digest_cons, blocksize):
+  outer = digest_cons()
+  inner = digest_cons()
+  if len(key) > blocksize:
+    key = digest_cons(key).digest()
+    # Usually inner.digest_size <= blocksize, so now len(key) < blocksize. 
+  key += '\0' * (blocksize - len(key))
+  outer.update(key.translate(hmac_trans_5C))
+  inner.update(key.translate(hmac_trans_36))
+  inner.update(msg)
+  outer.update(inner.digest())
+  return outer.digest()
+
+
+try:
+  import Crypto.Util.strxor
+  def make_strxor(size, strxor=Crypto.Util.strxor.strxor):
+    return strxor
+except ImportError:
+  # This is the naive implementation, it's too slow:
+  #
+  # def strxor(a, b, izip=itertools.izip):
+  #   return ''.join(chr(ord(x) ^ ord(y)) for x, y in izip(a, b))
+  # 
+  # 58 times slower pure Python implementation, see
+  # http://stackoverflow.com/a/19512514/97248
+  def make_strxor(size):
+    def strxor(a, b, izip=itertools.izip, pack=struct.pack, unpack=struct.unpack, fmt='%dB' % size):
+      return pack(fmt, *(a ^ b for a, b in izip(unpack(fmt, a), unpack(fmt, b))))
+    return strxor
+
+
+has_sha512_hashlib = has_sha512_openssl_hashlib = False
+try:
+  __import__('hashlib').sha512
+  has_sha512_hashlib = True
+  has_sha512_openssl_hashlib = __import__('hashlib').sha512.__name__.startswith('openssl_')
+except (ImportError, AttributeError):
+  pass
+has_sha512_pycrypto = False
+try:
+  __import__('Crypto.Hash._SHA512')
+  has_sha512_pycrypto = True
+except ImportError:
+  pass
+if has_sha512_openssl_hashlib:  # Fastest.
+  sha512 = sys.modules['hashlib'].sha512
+elif has_sha512_pycrypto:
+  # Faster than: Crypto.Hash.SHA512.SHA512Hash
+  sha512 = sys.modules['Crypto.Hash._SHA512'].new
+elif has_sha512_hashlib:
+  sha512 = sys.modules('hashlib').sha512
+else:
+  raise ImportError('Cannot find SHA512 implementation: install hashlib or pycrypto.')
+
+
+# Faster than `import pbkdf2' (available on pypi), because of less
+# indirection.
+def pbkdf2(passphrase, salt, size, iterations, digest_cons, blocksize):
+  """Computes an binary key from a passphrase using PBKDF2.
+
+  This is deliberately slow (to make dictionary-based attacks on passphrase
+  slower), especially when iterations is high.
+  """
+  # strxor is the slowest operation in pbkdf2. For example, for
+  # iterations=500000, digest_cons=sha512, len(passphrase) == 3, calls to
+  # strxor take 0.2s with Crypto.Util.strxor.strxor, and 11.6s with the pure
+  # Python make_strxor above. Other operations within the pbkdf2 call take
+  # about 5.9s if hashlib.sha512 is used, and 12.4s if
+  # Crypto.Hash._SHA512.new (also implemented in C) is used.
+
+  _do_hmac = do_hmac
+  key, i, k = [], 1, size
+  while k > 0:
+    u = previousu = _do_hmac(passphrase, salt + struct.pack('>I', i), digest_cons, blocksize)
+    _strxor = make_strxor(len(u))
+    for j in xrange(iterations - 1):
+      previousu = _do_hmac(passphrase, previousu, digest_cons, blocksize)
+      u = _strxor(u, previousu)
+    key.append(u)
+    k -= len(u)
+    i += 1
+  return ''.join(key)[:size]
 
 
 def check_decrypted_size(decrypted_size):
@@ -20,13 +114,32 @@ def check_keytable(keytable):
     raise ValueError('keytable must be 64 bytes, got: %d' % len(keytable))
 
 
+def check_header_key(header_key):
+  if len(header_key) != 64:
+    raise ValueError('header_key must be 64 bytes, got: %d' % len(header_key))
+
+
+def check_aes_xts_key(aes_xts_key):
+  if len(aes_xts_key) != 64:
+    raise ValueError('aes_xts_key must be 64 bytes, got: %d' % len(aes_xts_key))
+
+
+def check_dechd(dechd):
+  if len(dechd) != 512:
+    raise ValueError('dechd must be 512 bytes, got: %d' % len(dechd))
+
+
+def check_sector_size(sector_size):
+  if sector_size < 512 or sector_size & (sector_size - 1):
+    raise ValueError('sector_size must be a power of 2 at least 512: %d' % sector_size)
+
+
 def build_dechd(salt, keytable, decrypted_size, sector_size):
   check_keytable(keytable)
   check_decrypted_size(decrypted_size)
   if len(salt) != 64:
     raise ValueError('salt must be 64 bytes, got: %d' % len(salt))
-  if sector_size < 512 or sector_size & (sector_size - 1):
-    raise ValueError('sector_size must be a power of 2 at least 512: %d' % sector_size)
+  check_sector_size(sector_size)
   version = 5
   keytablep = keytable + '\0' * 192
   keytablep_crc32 = ('%08x' % (binascii.crc32(keytablep) & 0xffffffff)).decode('hex')
@@ -49,13 +162,66 @@ def build_dechd(salt, keytable, decrypted_size, sector_size):
   # 124 + 4: flag bits (0): 00000000
   # 128 + 4: sector size (512 -- shouldn't it be 4096?): 00000200
   # 132 + 120: zeros: 00..00
-  header = struct.pack('>4sHBB4s16xQQQQLL120x', 'VERA', header_format_version, minimum_version_to_extract[0], minimum_version_to_extract[1], keytablep_crc32, hidden_volume_size, decrypted_size, base_offset_for_key, decrypted_size, flag_bits, sector_size)
+  header = struct.pack(
+      '>4sHBB4s16xQQQQLL120x', 'VERA', header_format_version,
+      minimum_version_to_extract[0], minimum_version_to_extract[1],
+      keytablep_crc32, hidden_volume_size, decrypted_size,
+      base_offset_for_key, decrypted_size, flag_bits, sector_size)
   assert len(header) == 188
   header_crc32 = ('%08x' % (binascii.crc32(header) & 0xffffffff)).decode('hex')
   dechd = ''.join((salt, header, header_crc32, keytablep))
   assert len(dechd) == 512
   return dechd
 
+
+def check_full_dechd(dechd):
+  """Does a full, after-decryption check on dechd.
+
+  This is also used for passphrase: on a wrong passphrase, dechd is 512
+  bytes of garbage.
+
+  The checks here are more strict than what `cryptsetup' or the mount
+  operation of `veracrypt' does. They can be relaxed if the need arises.
+  """
+  check_dechd(dechd)
+  if dechd[64 : 64 + 4] != 'VERA':  # Or 'TRUE'.
+    raise ValueError('Magic mismatch.')
+  if dechd[72 : 76] != ('%08x' % (binascii.crc32(dechd[256 : 512]) & 0xffffffff)).decode('hex'):
+    raise ValueError('keytablep_crc32 mismatch.')
+  if dechd[252 : 256] != ('%08x' % (binascii.crc32(dechd[64 : 252]) & 0xffffffff)).decode('hex'):
+    raise ValueError('header_crc32 mismatch.')
+  header_format_version, = struct.unpack('>H', dechd[68 : 68 + 2])
+  if not (5 <= header_format_version <= 9):
+    raise ValueError('header_format_version mismatch.')
+  minimum_version_to_extract = struct.unpack('>BB', dechd[70 : 70 + 2])
+  if minimum_version_to_extract != (1, 11):
+    raise ValueError('minimum_version_to_extract mismatch.')
+  if dechd[76 : 76 + 16].lstrip('\0'):
+    raise ValueError('Missing NUL padding at 76.')
+  hidden_volume_size, = struct.unpack('>Q', dechd[92 : 92 + 8])
+  if hidden_volume_size:
+    # Hidden volume detected here, but currently this tool doesn't support
+    # hidden volumes.
+    raise ValueError('Unexpected hidden volume.')
+  decrypted_size, = struct.unpack('>Q', dechd[100 : 100 + 8])
+  if decrypted_size >> 50:  # Larger than 1 PB is insecure.
+    raise ValueError('Volume too large.')
+  base_offset_for_key, = struct.unpack('>Q', dechd[108 : 108 + 8])
+  if base_offset_for_key != 0x20000:
+    raise ValueError('base_offset_for_key mismatch.')
+  encrypted_area_size, = struct.unpack('>Q', dechd[116 : 116 + 8])
+  if encrypted_area_size != decrypted_size:
+    raise ValueError('encrypted_area_size mismatch.')
+  flag_bits, = struct.unpack('>L', dechd[124 : 124 + 4])
+  if flag_bits:
+    raise ValueError('flag_bits mismatch.')
+  sector_size, = struct.unpack('>L', dechd[128 : 128 + 4])
+  check_sector_size(sector_size)
+  if dechd[132 : 132 + 120].lstrip('\0'):
+    raise ValueError('Missing NUL padding at 132.')
+  if dechd[256 + 64 : 512].lstrip('\0'):
+    raise ValueError('Missing NUL padding after keytable.')
+  
 
 def build_table(keytable, decrypted_size, raw_device):
   check_keytable(keytable)
@@ -77,15 +243,71 @@ def build_table(keytable, decrypted_size, raw_device):
       iv_offset >> 9, raw_device, offset >> 9, opt_params_str)
 
 
-def work():
+# We use pure Python code (from CryptoPlus) for AES XTS encryption. This is
+# slow, but it's not a problem, because we have to encrypt only 512 bytes
+# per run. Please note that pycrypto-2.6.1 (released on 2013-10-17) and
+# other C crypto libraries with Python bindings don't support AES XTS.
+def crypt_aes_xts(aes_xts_key, data, do_encrypt):
+  check_aes_xts_key(aes_xts_key)
+  cipher = CryptoPlus.Cipher.python_AES.new((aes_xts_key[0 : 32], aes_xts_key[32 : 64]), CryptoPlus.Cipher.python_AES.MODE_XTS)
+  if do_encrypt:
+    return cipher.encrypt(data)
+  else:
+    return cipher.decrypt(data)
+
+
+def encrypt_header(dechd, header_key):
+  check_dechd(dechd)
+  check_header_key(header_key)
+  enchd = dechd[:64] + crypt_aes_xts(header_key, dechd[64 : 512], do_encrypt=True)
+  assert len(enchd) == 512
+  return enchd
+
+
+def decrypt_header(enchd, header_key):
+  if len(enchd) != 512:
+    raise ValueError('enchd must be 512 bytes, got: %d' % len(enchd))
+  check_header_key(header_key)
+  dechd = enchd[:64] + crypt_aes_xts(header_key, enchd[64 : 512], do_encrypt=False)
+  assert len(dechd) == 512
+  return dechd
+
+
+# Slow, takes about 6..60 seconds.
+def build_header_key(passphrase, salt_or_enchd):
+  if len(salt_or_enchd) < 64:
+    raise ValueError('Salt too short.')
+  salt = salt_or_enchd[:64]
+  iterations = 500000
+  # We could use a different hash algorithm and a different iteration count.
+  header_key_size = 64
+  #blocksize = 16  # For MD2
+  #blocksize = 64  # For MD4, MD5, RIPEMD, SHA1, SHA224, SHA256.
+  #blocksize = 128  # For SHA384, SHA512.
+  sha512_blocksize = 128
+  return pbkdf2(passphrase, salt, header_key_size, iterations, sha512, sha512_blocksize)
+
+
+def parse_dechd(dechd):
+  check_dechd(dechd)
+  keytable = dechd[256 : 256 + 64]
+  decrypted_size, = struct.unpack('>Q', dechd[100 : 100 + 8])
+  return keytable, decrypted_size
+
+
+def test():
+  # !! Is it always 500000 iterations? Does it depend on the passphrase length.
   passphrase = 'foo'
   raw_device = '7:0'
   decrypted_size = 0x9000
   sector_size = 512
+  # Any 64 random bytes will do as a salt.
   salt = 'd97538ba99ca3182fd9e46184801a836a83a245f703247987dbd8d5c6a39ff5fbc4d03942ec54401d109d407c8033ede03930c95ddcc61b5b44ce3de6cac8b44'.decode('hex')
+  # Any 64 random bytes will do as a keytable.
   keytable = 'a64cd0845765a19b0b5948f371f0b8c7b14da01677a10009d8b9199d511624233a54e1118dd6c9e2992e3ebae56081ca1f996c74c53f61f1a48f7fb17ddc6d5b'.decode('hex')
   enchd = salt + '55ade210c4de6bbf5f623fb944908f0b4952958188dbe9ff0723cc6d6e1fdbf9554f4c9a0bbb4f49066641911ccbcb212234a9e677de9404d58950f5eceab3b9d2b290c071e4c74ee848af4ec2d730b13ded8d9bce64b92786b6eaa1c5abe23f23601a2f4ce30283c791f571548ef30b3b32c4558ec102a96176eea3864e3c3bd0f853e55df2de9125c4e782aca78479065839d7878122d9dc5ac8af8626218a3f74ca327a79b61d0cee6f8c4c5972bd53a87fdb7732a86f775e7f6c7ac801b79fa75759554dce512daa6bc4444b49907fa8adb7e5f14963aa8a6a8a3a5bf51b549a7d7569d641331749e88f453163a56a7a3c7f46375b3adfba9f30be9c41200dd9779eaf52220e732f3e4c7ee9c501e63ccd9c6f53bbb70f649c08d64eb740e034e26cdf8dd8209b2e8da9aac90dab3005215410c48109f263e50ba1fa736fd2de0b252bc008f2f1eab2e0fb42c5579bab32ac86686cc264181790c3426eb16dcbdea12f708758e19bbae1072ef7157cef87fd8722f2d2eca8a85510b83ea3d534031e38e018f8554944681885f7d912760d449bca4fbc39ff9bd2c2192f71550b131b2a2afe6371c7c122e6f5c865cb2cbbf889d2ce54da9f55a2000cf4e0'.decode('hex')
-  dechd = salt + '564552410005010b5741849c0000000000000000000000000000000000000000000000000000000000009000000000000002000000000000000090000000000000000200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b2df12c0a64cd0845765a19b0b5948f371f0b8c7b14da01677a10009d8b9199d511624233a54e1118dd6c9e2992e3ebae56081ca1f996c74c53f61f1a48f7fb17ddc6d5b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'.decode('hex')
+  dechd = salt + ('564552410005010b5741849c0000000000000000000000000000000000000000000000000000000000009000000000000002000000000000000090000000000000000200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b2df12c0' + keytable.encode('hex') + '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000').decode('hex')
+  header_key = '9e02d6ca37ac50a97093b3323545ec1cd9d11e03bfdaf123043bf1c42df5b6fc6660a2313e087fa80775942db79a9f297670f01ea6d555baa8599028cd8c8094'.decode('hex')
 
   assert len(salt) == 64
   assert len(enchd) == 512
@@ -93,35 +315,44 @@ def work():
   assert dechd.startswith(salt)
   assert enchd.startswith(salt)
   assert dechd[256 : 256 + 64] == keytable
-  #print 'calc  ', ('%08x' % (binascii.crc32(dechd[64 : 252]) & 0xffffffff)).decode('hex')[::-1].encode('hex')  # !! mismatch
-  #print 'stored', dechd[252 : 256].encode('hex')
-  assert ('%08x' % (binascii.crc32(dechd[256 : 512]) & 0xffffffff)).decode('hex') == dechd[72 : 76]
-  assert ('%08x' % (binascii.crc32(dechd[64 : 252]) & 0xffffffff)).decode('hex') == dechd[252 : 256]
 
-  dechd2 = build_dechd(salt, keytable, decrypted_size, sector_size)
-  assert dechd2 == dechd
+  check_full_dechd(dechd)
+  assert build_dechd(salt, keytable, decrypted_size, sector_size) == dechd
+  assert parse_dechd(dechd) == (keytable, decrypted_size)
   table = build_table(keytable, decrypted_size, raw_device)
   expected_table = '0 72 crypt aes-xts-plain64 a64cd0845765a19b0b5948f371f0b8c7b14da01677a10009d8b9199d511624233a54e1118dd6c9e2992e3ebae56081ca1f996c74c53f61f1a48f7fb17ddc6d5b 256 7:0 256 1 allow_discards\n'
   assert table == expected_table
+  assert encrypt_header(dechd, header_key) == enchd
+  assert decrypt_header(enchd, header_key) == dechd
+  print >>sys.stderr, 'test continuing 1.'
+  assert build_header_key(passphrase, salt) == header_key  # Takes about 6..60 seconds.
+  print >>sys.stderr, 'test OK.'
 
-  print 'OK1'
 
-  import CryptoPlus.Cipher.python_AES  
-  cipher = CryptoPlus.Cipher.python_AES.new('2b7e151628aed2a6abf7158809cf4f3c'.decode('hex'))
-  crypted = cipher.encrypt('6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51'.decode('hex'))
-  assert crypted.encode('hex') == '3ad77bb40d7a3660a89ecaf32466ef97f5d3d58503b9699de785895a96fdbaaf'
+def main(argv):
+  raw_device = '7:0'
 
-  # !! TODO(pts): How do we derive the key from the passphrase.
-  # !! How do we generate a good keytable? Is it just random?
-  key = '9e02d6ca37ac50a97093b3323545ec1cd9d11e03bfdaf123043bf1c42df5b6fc6660a2313e087fa80775942db79a9f297670f01ea6d555baa8599028cd8c8094'.decode('hex')
-  assert len(key) == 64
-  key = 
-  cipher = CryptoPlus.Cipher.python_AES.new((key[0 : 32], key[32 : 64]), CryptoPlus.Cipher.python_AES.MODE_XTS)
-  assert cipher.decrypt(enchd[64:]) == dechd[64:]
-  assert cipher.encrypt(dechd[64:]) == enchd[64:]
+  #device = 'pp.bin'
+  passphrase = 'ThisIsMyVeryLongPassphraseForMyVeraCryptVolume'
 
-  print 'OK2'
+  #device = '../pts-static-cryptsetup/rr.bin'
+
+  enchd = open(device).read(512)
+  if len(enchd) != 512:
+    raise ValueError('Raw device too short for VeraCrypt header.')
+  header_key = build_header_key(passphrase, enchd)  # Slow.
+  dechd = decrypt_header(enchd, header_key)
+  try:
+    check_full_dechd(dechd)
+  except ValueError, e:
+    # We may put str(e) to the debug log, if requested.
+    raise ValueError('Incorrect passphrase.')
+  keytable, decrypted_size = parse_dechd(dechd)
+  table = build_table(keytable, decrypted_size, raw_device)
+  sys.stdout.write(table)
+  sys.stdout.flush()
 
 
 if __name__ == '__main__':
-  work()
+  #test()
+  sys.exit(main(sys.argv))
