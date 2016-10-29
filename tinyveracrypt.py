5e6424082891d89378035bed0edea43ce771b20d
Peter Szabo <pts@fazekas.hu>
2016-10-29 18:49:40 +0100

    updated tinyveracrypt.py; added tinyveracrypt_test.py

--- tinyveracrypt.py
+++ tinyveracrypt.py
@@ -1,6 +1,18 @@
-#! /usr/bin/python
-# by pts@fazekas.hu at Fri Oct 28 16:32:11 CEST 2016
-# !! Which Python version works?
+#! /bin/sh
+# by pts@fazekas.hu at Sat Oct 29 19:43:26 CEST 2016
+
+""":" #tinyveracrypt: VeraCrypt-compatible block device encryption setup
+
+type python2.7 >/dev/null 2>&1 && exec python2.7 -- "$0" ${1+"$@"}
+type python2.6 >/dev/null 2>&1 && exec python2.6 -- "$0" ${1+"$@"}
+type python2.5 >/dev/null 2>&1 && exec python2.5 -- "$0" ${1+"$@"}
+type python2.4 >/dev/null 2>&1 && exec python2.4 -- "$0" ${1+"$@"}
+exec python -- ${1+"$@"}; exit 1
+
+This script works with Python 2.5, 2.6 and 2.7 out of the box, and with
+Python 2.4 if the hashlib is installed from PyPi. It doesn't work with older
+versions of Python or Python 3.x.
+"""
 
 import binascii
 import itertools
@@ -480,6 +492,8 @@ elif has_sha512_pycrypto:
 elif has_sha512_hashlib:
   sha512 = sys.modules('hashlib').sha512
 else:
+  # Using a pure Python implementation here would be too slow, because
+  # sha512 is used in pbkdf2.
   raise ImportError('Cannot find SHA512 implementation: install hashlib or pycrypto.')
 
 
@@ -705,46 +719,10 @@ def get_table(device, passphrase, raw_device):
   return build_table(keytable, decrypted_size, raw_device)
 
 
-def test():
-  assert crypt_aes_xts('x' * 64, 'y' * 32, True).encode('hex') == '622de15539f9ebe251c97183c1618b2fe6926943919e0fc5385306027a473c58'
-  assert crypt_aes_xts('x' * 64, 'y' * 35, True).encode('hex') == '622de15539f9ebe251c97183c1618b2fa1289ef677ad71945095f99a59d7c366e69269'
-
-  passphrase = 'foo'
-  raw_device = '7:0'
-  decrypted_size = 0x9000
-  sector_size = 512
-  # Any 64 random bytes will do as a salt.
-  salt = 'd97538ba99ca3182fd9e46184801a836a83a245f703247987dbd8d5c6a39ff5fbc4d03942ec54401d109d407c8033ede03930c95ddcc61b5b44ce3de6cac8b44'.decode('hex')
-  # Any 64 random bytes will do as a keytable.
-  keytable = 'a64cd0845765a19b0b5948f371f0b8c7b14da01677a10009d8b9199d511624233a54e1118dd6c9e2992e3ebae56081ca1f996c74c53f61f1a48f7fb17ddc6d5b'.decode('hex')
-  enchd = salt + '55ade210c4de6bbf5f623fb944908f0b4952958188dbe9ff0723cc6d6e1fdbf9554f4c9a0bbb4f49066641911ccbcb212234a9e677de9404d58950f5eceab3b9d2b290c071e4c74ee848af4ec2d730b13ded8d9bce64b92786b6eaa1c5abe23f23601a2f4ce30283c791f571548ef30b3b32c4558ec102a96176eea3864e3c3bd0f853e55df2de9125c4e782aca78479065839d7878122d9dc5ac8af8626218a3f74ca327a79b61d0cee6f8c4c5972bd53a87fdb7732a86f775e7f6c7ac801b79fa75759554dce512daa6bc4444b49907fa8adb7e5f14963aa8a6a8a3a5bf51b549a7d7569d641331749e88f453163a56a7a3c7f46375b3adfba9f30be9c41200dd9779eaf52220e732f3e4c7ee9c501e63ccd9c6f53bbb70f649c08d64eb740e034e26cdf8dd8209b2e8da9aac90dab3005215410c48109f263e50ba1fa736fd2de0b252bc008f2f1eab2e0fb42c5579bab32ac86686cc264181790c3426eb16dcbdea12f708758e19bbae1072ef7157cef87fd8722f2d2eca8a85510b83ea3d534031e38e018f8554944681885f7d912760d449bca4fbc39ff9bd2c2192f71550b131b2a2afe6371c7c122e6f5c865cb2cbbf889d2ce54da9f55a2000cf4e0'.decode('hex')
-  dechd = salt + ('564552410005010b5741849c0000000000000000000000000000000000000000000000000000000000009000000000000002000000000000000090000000000000000200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000b2df12c0' + keytable.encode('hex') + '00' * 192).decode('hex')
-  header_key = '9e02d6ca37ac50a97093b3323545ec1cd9d11e03bfdaf123043bf1c42df5b6fc6660a2313e087fa80775942db79a9f297670f01ea6d555baa8599028cd8c8094'.decode('hex')
-
-  assert len(salt) == 64
-  assert len(enchd) == 512
-  assert len(dechd) == 512
-  assert dechd.startswith(salt)
-  assert enchd.startswith(salt)
-  assert dechd[256 : 256 + 64] == keytable
-
-  check_full_dechd(dechd)
-  assert build_dechd(salt, keytable, decrypted_size, sector_size) == dechd
-  assert parse_dechd(dechd) == (keytable, decrypted_size)
-  table = build_table(keytable, decrypted_size, raw_device)
-  expected_table = '0 72 crypt aes-xts-plain64 a64cd0845765a19b0b5948f371f0b8c7b14da01677a10009d8b9199d511624233a54e1118dd6c9e2992e3ebae56081ca1f996c74c53f61f1a48f7fb17ddc6d5b 256 7:0 256 1 allow_discards\n'
-  assert table == expected_table
-  assert encrypt_header(dechd, header_key) == enchd
-  assert decrypt_header(enchd, header_key) == dechd
-  print >>sys.stderr, 'info: test continuing 1.'
-  assert build_header_key(passphrase, salt) == header_key  # Takes about 6..60 seconds.
-  print >>sys.stderr, 'info: test OK.'
-
-
 def main(argv):
   raw_device = '7:0'
 
-  #device = 'pp.bin'
+  device = 'pp.bin'
   passphrase = 'ThisIsMyVeryLongPassphraseForMyVeraCryptVolume'
 
   #device = '../pts-static-cryptsetup/rr.bin'
@@ -754,5 +732,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-  test()
-  #sys.exit(main(sys.argv))
+  sys.exit(main(sys.argv))
