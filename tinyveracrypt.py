0f62a217b6f2316db95708b99d9939dca37bcc47
Peter Szabo <pts@fazekas.hu>
2018-12-25 16:42:10 +0100

    added faster fallback implementation of strxor using bytearray

--- tinyveracrypt.py
+++ tinyveracrypt.py
@@ -26,17 +26,27 @@ try:
   def make_strxor(size, strxor=sys.modules['Crypto.Util.strxor'].strxor):
     return strxor
 except ImportError:
-  # This is the naive implementation, it's too slow:
-  #
-  # def strxor(a, b, izip=itertools.izip):
-  #   return ''.join(chr(ord(x) ^ ord(y)) for x, y in izip(a, b))
-  #
-  # 58 times slower pure Python implementation, see
-  # http://stackoverflow.com/a/19512514/97248
-  def make_strxor(size):
-    def strxor(a, b, izip=itertools.izip, pack=struct.pack, unpack=struct.unpack, fmt='%dB' % size):
-      return pack(fmt, *(a ^ b for a, b in izip(unpack(fmt, a), unpack(fmt, b))))
-    return strxor
+  try:
+    bytearray  # Introduced in Python 2.6. Raises NameError in older Python.
+    # This is about 26.18% faster than the one below using pack.
+    #
+    # Using array.array('B', ...) would be a bit slower than using pack.
+    def make_strxor(size):
+      def strxor(a, b, izip=itertools.izip, ba=bytearray, st=str):
+        return st(ba((a ^ b for a, b in izip(ba(a), ba(b)))))
+      return strxor
+  except NameError:
+    # This is the naive implementation, it's too slow:
+    #
+    # def strxor(a, b, izip=itertools.izip):
+    #   return ''.join(chr(ord(x) ^ ord(y)) for x, y in izip(a, b))
+    #
+    # 58 times slower pure Python implementation, see
+    # http://stackoverflow.com/a/19512514/97248
+    def make_strxor(size):
+      def strxor(a, b, izip=itertools.izip, pack=struct.pack, unpack=struct.unpack, fmt='%dB' % size):
+        return pack(fmt, *(a ^ b for a, b in izip(unpack(fmt, a), unpack(fmt, b))))
+      return strxor
 
 
 # ---  AES XTS crypto code.
